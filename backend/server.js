29aeb28124a0051c75db61100233c0f9674832c8
Avni34 <sharmaavni842@gmail.com>
2024-07-21 21:28:07 +0530

    auth route setup

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,10 @@ app.get("/", (req,res) => {
     res.send("Helloo")
 })
 
+import authRoutes from "../routes/auth.routes.js"
+
+app.use("/api/auth", authRoutes)
+
 app.listen(4000, () => {
     console.log("server running on port " + PORT)
 })
